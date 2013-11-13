902d1b4d6fe47c9b62407056f8afbc4774909141
Luke <Luke@7eb0a0fd-2b4e-5d48-be3d-a80426e74fde>
2013-11-13 22:17:43 +0000

    Added main with splash screen.
    git-svn-id: svn://localhost/trunk@28 7eb0a0fd-2b4e-5d48-be3d-a80426e74fde

---
+++ main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# Original Author: Luke McNinch
+# Original Creation Date: 2013/11/13
+# $Rev$
+# $LastChangedDate$
+__version__ = '$Id$'.replace('$', '')
+
+import sys
+from PyQt4.QtGui import QApplication, QPixmap, QSplashScreen
+from PyQt4.QtCore import Qt
+
+
+"""
+This script imports the minimum modules necessary to display a splash
+screen before importing and displaying the downloader application.
+"""
+
+app = QApplication(sys.argv)
+
+# Create and display the splash screen
+splash_pix = QPixmap('icons\splash_loading.png')
+splash = QSplashScreen(splash_pix, Qt.WindowStaysOnTopHint)
+splash.setMask(splash_pix.mask())
+splash.show()
+app.processEvents()
+
+# Import and display the application
+from mileage_gui import mileageGui
+myapp = mileageGui()
+myapp.show()
+myapp.Open('../FuelRecord.csv')
+
+# Close the splash screen
+splash.finish(myapp)
+
+app.exec_()
